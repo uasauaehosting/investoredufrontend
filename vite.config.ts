7efd30f61571c17501ea7a8f78dd499197419ea2
Future Tech Admin <artcreativity2020@gmail.com>
2026-06-14 09:47:59 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://apiinvestoredu.ahwuae.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
